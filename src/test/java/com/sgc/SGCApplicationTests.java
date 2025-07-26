584d88ec167481de099f6013881093134f5ed5c6
branco0verflow <brann455@gmail.com>
2025-07-25 21:43:58 -0300

    Ajuste variables

--- src/test/java/com/sgc/SGCApplicationTests.java
+++ src/test/java/com/sgc/SGCApplicationTests.java
@@ -1,10 +1,12 @@
 package com.sgc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SGCApplicationTests {
+
 
 	@Test
 	void contextLoads() {
