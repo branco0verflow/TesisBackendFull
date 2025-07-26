97bc8b6cd645626d70f1b71303c6511f0ae90beb
branco0verflow <brann455@gmail.com>
2025-07-25 22:46:59 -0300

    Se ajustaron rutas fetch

--- src/main/java/com/sgc/config/WebConfig.java
+++ src/main/java/com/sgc/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://tesis-taller-front.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
